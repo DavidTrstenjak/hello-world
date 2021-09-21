fb3d83ba6d7200f0923e16c562e5f50e77a6f54d
DavidTrstenjak <90246263+DavidTrstenjak@users.noreply.github.com>
2021-09-21 10:50:00 +0200

    Create helloWorld.cs

---
+++ helloWorld.cs
@@ -0,0 +1,16 @@
+/*
+*David Trstenjak
+*3.D
+*21.9.2021.
+*hello world
+*/
+
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Console.WriteLine("Hello World");
+	}
+}
